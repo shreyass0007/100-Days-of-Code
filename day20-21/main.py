a3a8a6695672bf65c3a5b53bdbd4c0e8845c5a93
shreyass0007 <shreshshende.777@gmail.com>
2025-06-18 12:43:31 +0530

    extending snake

--- day20-21/main.py
+++ day20-21/main.py
@@ -33,7 +33,15 @@ while game_is_on:
     if snake.head.distance(food)<15:
         
         food.refresh()
+        snake.extend()
         score.increase_score()
+
+    #Detect collision with wall
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        game_is_on=False
+        score.game_over()
+
+
 
 
 screen.exitonclick()
--- day20-21/snake.py
+++ day20-21/snake.py
@@ -16,11 +16,23 @@ class Snake:
     
     def create_snake(self):
         for position in STARTING_POSITION:  # Fixed typo in variable name
+            self.add_segment(position)
+            
+
+    def add_segment(self,position):
             new_segment = Turtle('square')
             new_segment.color('white')
             new_segment.penup()
             new_segment.goto(position)  # Fixed typo in variable name
             self.segments.append(new_segment)
+
+   
+
+    def extend(self):
+        #add a new segment to the snake
+        self.add_segment(self.segments[-1].position())
+
+
 
     def move(self):
         for seg_num in range(len(self.segments)-1,0,-1):
