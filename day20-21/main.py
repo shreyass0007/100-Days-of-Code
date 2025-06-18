896b1548c1493f74aba3491e208ffaf388102e13
shreyass0007 <shreshshende.777@gmail.com>
2025-06-18 11:28:46 +0530

    adding food for snake

---
+++ day20-21/main.py
@@ -0,0 +1,35 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+food=Food()
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+ # Fixed typo in variable name
+
+
+game_is_on = True
+while game_is_on:
+    snake.move()
+    screen.update()
+    time.sleep(0.1)
+
+   #Detect collison with food
+    if snake.head.distance(food)<15:
+        food.refresh()
+
+
+screen.exitonclick()
