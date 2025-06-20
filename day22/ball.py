fb8b2c9820cca99e4c310ad6d7874e745dd3c5a0
shreyass0007 <shreshshende.777@gmail.com>
2025-06-20 13:14:10 +0530

    score biboard updated

--- day22/ball.py
+++ day22/ball.py
@@ -8,7 +8,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move=10
         self.y_move=10
-        
+        self.move_speed=0.1
     def move(self):
         new_x=self.xcor()+self.x_move
         new_y=self.ycor()+self.y_move
@@ -16,11 +16,14 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move*=-1
+        self.move_speed*=0.9
 
     def bounce_x(self):
         self.x_move*=-1
 
     def reset_position(self):
+
         self.goto(0,0)
+        self.move_sped=0.1
         self.bounce_x()
 
