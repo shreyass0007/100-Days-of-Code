8345a17c7585f283f1d46f79468e7a2055141936
shreyass0007 <shreshshende.777@gmail.com>
2025-06-21 19:38:53 +0530

    Creating random car

--- day23/turtle-crossing-start/car_manager.py
+++ day23/turtle-crossing-start/car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        all_cars=[]
+    
+    def create_cars(self):
+        new_car=Turtle("square")
+        new_car.shapesize(stretch_wid=2,stretch_len=1)
+        new_car.penup()
+        new_car.color(random.choice())
+        random_Y=random.randint(-250,250)
+        new_car.goto(300,random_Y)
+        self.all_cars.append(new_car)
+    
+         
