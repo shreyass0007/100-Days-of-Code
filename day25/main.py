17f577943a5e9cd424cc1c40bdabbade356b95f6
shreyass0007 <shreshshende.777@gmail.com>
2025-06-24 10:44:35 +0530

    video 1 pands and csv complete

--- day25/main.py
+++ day25/main.py
@@ -15,7 +15,32 @@ import pandas as pd
 df=pd.read_csv("D:/CODEING/100 days of python/day25/weather_data - Sheet1.csv")
 # print(type(df["temp"]))
 
-data_dict=df.to_dict()
-print(data_dict)
+# data_dict=df.to_dict()
+# print(data_dict)
 
+# temp_list=df["temp"].to_list()
+# print(temp_list)
+# # avg=sum(temp_list)/len(temp_list)
+# # print(avg)
 
+# print(df["temp"].mean())
+# print(df["temp"].max())
+
+# #Get Data in colums
+# print(df["condition"])
+# print(df.condition)
+
+#Get data in rows
+print(df[df.day=="Monday"])
+print(df.temp==df["temp"].max())
+
+#create Dataframe form scractch
+
+data_dict={
+    "students":["amy","James","angela"],
+    "scores":[76,56,65]
+}
+data=pd.DataFrame(data_dict)
+#creating csv files
+data.to_csv("new_Data.csv")
+print(data)
