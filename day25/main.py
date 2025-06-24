0202f61e1a4bce0eacf1c1e7e475516097cc10a0
shreyass0007 <shreshshende.777@gmail.com>
2025-06-24 09:35:26 +0530

    learning pandas

--- day25/main.py
+++ day25/main.py
@@ -1,13 +1,18 @@
 # with open("D:/CODEING/100 days of python/day25/weather_data - Sheet1.csv") as word:
 #     print(word.readlines())
 
-import csv
-with open("D:/CODEING/100 days of python/day25/weather_data - Sheet1.csv") as data_file:
-    data=csv.reader(data_file)
-    temperature=[]
-    for row in data:
-        print(row)
-        if row[1]!="temp":
-            temperature.append(int(row[1]))
-    print(temperature)
+# import csv
+# with open("D:/CODEING/100 days of python/day25/weather_data - Sheet1.csv") as data_file:
+#     data=csv.reader(data_file)
+#     temperature=[]
+#     for row in data:
+#         print(row)
+#         if row[1]!="temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+import pandas as pd
+df=pd.read_csv("D:/CODEING/100 days of python/day25/weather_data - Sheet1.csv")
+print(df["temp"])
+type(df)
 
