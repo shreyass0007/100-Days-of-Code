a4f9bd2573e573a249c88ab0c9d6ee251e9bae97
shreyass0007 <shreshshende.777@gmail.com>
2025-06-25 11:09:41 +0530

    updated program

--- day25/US-states game/main.py
+++ day25/US-states game/main.py
@@ -13,10 +13,19 @@ guessed_states=[]
 
 while len(guessed_states)<50:
 
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/ 50 States correct", prompt="What's another state's name?").title()
     print(answer_state)
 
     # If answer_state is one of the states in all the states of the 50_states.csv
+    if answer_state=="Exit":
+        missing_states=[]
+        for  state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data=pd.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")
+
+        break
     if answer_state in all_states:
         # If they got it right:
         guessed_states.append(answer_state)
@@ -27,4 +36,5 @@ while len(guessed_states)<50:
         t.goto(int(state_data.x), int(state_data.y))  # Fixed: access x and y values correctly
         t.write(state_data.state.item())  # Just write the state name we already have
 
-screen.exitonclick()
+
+#states to learn.csv
