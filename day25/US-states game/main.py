112450dcbd525d0c85812a8807c2a68ae8735058
shreyass0007 <shreshshende.777@gmail.com>
2025-06-25 10:19:50 +0530

    starting project

--- day25/US-states game/main.py
+++ day25/US-states game/main.py
@@ -1,9 +1,13 @@
 import turtle
+import pandas as pd
 screen=turtle.Screen()
 screen.title("U.S.States Game")
 image="blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+pd.read_csv("50_states.csv")
+
 answer_State=screen.textinput(title="Guess the State",prompt="what's another state's name?")
 print(answer_State)
 screen.exitonclick()
