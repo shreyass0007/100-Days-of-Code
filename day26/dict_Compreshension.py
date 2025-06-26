5a867f3f856dded9790f8954a2430e890964dcbb
shreyass0007 <shreshshende.777@gmail.com>
2025-06-26 10:44:58 +0530

    learning pandas

--- day26/dict_Compreshension.py
+++ day26/dict_Compreshension.py
@@ -5,3 +5,5 @@ import random
 names=['Alex','Beth','Caroline','Dave','Eleanor','Freddie']
 students_score={student:random.randint(1,100) for student in names}
 print(students_score)
+passed_students={student:score for (student,score) in students_score.items() if score>=60}
+print(passed_students)
