fb8977823398ab75aa45f2164b194e4bd00e3573
shreyass0007 <shreshshende.777@gmail.com>
2025-06-26 09:33:14 +0530

    understanding the dict

---
+++ day26/dict_Compreshension.py
@@ -0,0 +1,7 @@
+#Dictionary Comprehension
+#new_dict={new_key:new_value for item in list}
+#new_dict={new_key:new_value for (key,value) in dict.items()}
+import random
+names=['Alex','Beth','Caroline','Dave','Eleanor','Freddie']
+students_score={student:random.randint(1,100) for student in names}
+print(students_score)
