a02bfda710b49837662213f017615ae00380154a
shreyass0007 <shreshshende.777@gmail.com>
2025-06-27 10:29:43 +0530

    understanding pack() place() gride()

--- day27/main.py
+++ day27/main.py
@@ -4,9 +4,12 @@ window.title("My First GUI")
 window.minsize(width=500,height=300)
 #label
 my_label=Label(text="I am a label",font=("Arial",24,"bold"))
-my_label.pack()
+# my_label.pack()
 
 my_label["text"]="New text"
+my_label.config(text="New text")
+my_label.grid(column=0,row=0)
+
 input=Entry(width=10)
 #button 
 def button_clicked():
@@ -14,23 +17,9 @@ def button_clicked():
     new_text=input.get()
     my_label['text']=new_text
 button=Button(text="Click me",command=button_clicked)
-button.pack()
+button.grid(column=1,row=1)
 
 #Entry
-
-
-input.pack()
-
-
-
-
-
-
-
-
-
-
-
-
+input.grid(column=3,row=3)
 
 window.mainloop()
