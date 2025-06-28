d4895dae13785377771e8822640e1915184b03ad
shreyass0007 <shreshshende.777@gmail.com>
2025-06-28 10:01:00 +0530

    Adding timer

--- day28/main.py
+++ day28/main.py
@@ -15,18 +15,29 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def count_down(count):
+    canvas.itemconfig(timer_text,text=count)
+    if(count>0):
+        window.after(1000,count_down,count-1)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window=Tk()
 window.title("Pomodoro")
 
 window.config(padx=100,pady=50,bg=YELLOW)
+
+
+# def say_something(thing):
+#     print(thing)
+# window.after(1000,say_something,"hello")
 tomato_img=PhotoImage(file="tomato.png")
 canvas=Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+timer_text=canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
 
+count_down(5)
 start_button=button = Button(text="Start")
 start_button.grid(column=0,row=2)
 
