604091a8eda11c4ca0da2ee6c6d11641d4e6e356
shreyass0007 <shreshshende.777@gmail.com>
2025-06-29 17:47:09 +0530

    project update

--- day30/NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
+++ day30/NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
@@ -8,10 +8,12 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-
-word = input("Enter a word: ").upper()
-try:
-    output_list = [phonetic_dict[letter] for letter in word]
-    print(output_list)
-except:
-    print("Sorry, only letter in alphabet please")
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        
+    except KeyError:
+        print("Sorry, only letter in alphabet please")
+    else:
+        print(output_list)
