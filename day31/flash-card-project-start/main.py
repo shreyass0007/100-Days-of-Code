e24a925ef815158aae995f55c13654323b2b185b
shreyass0007 <shreshshende.777@gmail.com>
2025-07-01 10:42:18 +0530

    flip card

--- day31/flash-card-project-start/main.py
+++ day31/flash-card-project-start/main.py
@@ -3,10 +3,27 @@ import pandas as pd
 import random
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+
+def next_card():
+    global current_card
+    current_card=random.choice(data_dict)
+    
+    
+    canvas.itemconfig(card_title,text="French")
+    canvas.itemconfig(card_word,text=current_card["French"])
+
+def flip_card():
+    global current_card
+    canvas.itemconfig(card_title,text="English")
+    canvas.itemconfig(card_word,text=current_card["English"])
+
+    
 window = Tk()
 window.title("Flashy")
 window.minsize(width=800, height=526)
 window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+window.after(2000,func=flip_card)
 
 #canvas setup
 
@@ -15,13 +32,8 @@ card_img = PhotoImage(file="card_front.png")
 canvas.create_image(400,263,image=card_img)
 data=pd.read_csv("day31/flash-card-project-start/data/french_words.csv")
 data_dict=data.to_dict(orient="records")
-print(data_dict)
-def next_card():
-    current_card=random.choice(data_dict)
-    current_card=current_card["French"]
-    
-    canvas.itemconfig(card_title,text="French")
-    canvas.itemconfig(card_word,text=current_card)
+current_card={}
+
 
 
 
@@ -37,7 +49,6 @@ card_title=canvas.create_text(400,150,text="",font=("Arial",40,"italic"))
 card_word=canvas.create_text(400,263,text="",font=("Arial",60,"bold"))
 
 canvas.grid(row=0,column=0,columnspan=2)
-french_word=pd.read_csv("day31/flash-card-project-start/data/french_words.csv")
 
 next_card()
 
