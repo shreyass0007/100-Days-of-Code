6d94c82067e41280ca2494abad8f02c2c8400e42
shreyass0007 <shreshshende.777@gmail.com>
2025-07-01 14:31:12 +0530

    if the word learn it remove form list

--- day31/flash-card-project-start/main.py
+++ day31/flash-card-project-start/main.py
@@ -3,11 +3,14 @@ import pandas as pd
 import random
 BACKGROUND_COLOR = "#B1DDC6"
 
+data=pd.read_csv("day31/flash-card-project-start/data/french_words.csv")
+to_learn=data.to_dict(orient="records")
+current_card={}
 
 
 def next_card():
     global current_card,flip_timer
-    current_card=random.choice(data_dict)
+    current_card=random.choice(to_learn)
     canvas.itemconfig(card_title,text="French",fill="black")
     canvas.itemconfig(card_word,text=current_card["French"],fill="black")
     canvas.itemconfig(card_background,image=card_img)
@@ -18,6 +21,10 @@ def flip_card():
     canvas.itemconfig(card_word,text=current_card["English"],fill="white")
     canvas.itemconfig(card_background,image=back_img)
 
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    next_card()
     
 window = Tk()
 window.title("Flashy")
@@ -31,8 +38,7 @@ canvas=Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightbackground=BACKG
 card_img = PhotoImage(file="card_front.png")
 card_background=canvas.create_image(400,263,image=card_img)
 back_img=PhotoImage(file="card_back.png")
-data=pd.read_csv("day31/flash-card-project-start/data/french_words.csv")
-data_dict=data.to_dict(orient="records")
+
 current_card={}
 
 
@@ -43,7 +49,7 @@ wrong_button=Button(image=wrong_img, highlightthickness=0,command=next_card)
 wrong_button.grid(row=1,column=0)
 
 Right_img = PhotoImage(file="right.png")
-Right_button=Button(image=Right_img, highlightthickness=0,command=next_card)
+Right_button=Button(image=Right_img, highlightthickness=0,command=is_known)
 Right_button.grid(row=1,column=1)
 
 card_title=canvas.create_text(400,150,text="",font=("Arial",40,"italic"))
