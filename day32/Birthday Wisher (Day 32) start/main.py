58932b3f13f69e8305955776602a6929c5ce7c64
shreyass0007 <shreshshende.777@gmail.com>
2025-07-01 19:02:19 +0530

    sending quote to mail

--- day32/Birthday Wisher (Day 32) start/main.py
+++ day32/Birthday Wisher (Day 32) start/main.py
@@ -1,22 +1,31 @@
-# import smtplib
-
-# my_email = "shreshshende.777@gmail.com"
-# password = "ywsgnsgssomczuce"  # This is an app password, right?
-
-# # Using SMTP_SSL (port 465) - no need for starttls()
-# with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="shreyasshende.999@gmail.com",
-#         msg="Subject:Hello\n\nThis is the email body."
-#     )
-
+import smtplib
+import random
 import datetime as dt
+
+my_email = "shreshshende.777@gmail.com"
+password = "ywsgnsgssomczuce"  # This is an app password, right?
+
 now=dt.datetime.now()
-year=now.year
 day_of_week=now.weekday()
 print(day_of_week)
-date_of_birth=dt.datetime(year=1995,month=12,day=15)
-print(date_of_birth)
+
+def get_quote():
+    with open("day32/Birthday Wisher (Day 32) start/quotes.txt") as file:
+        lines=file.readlines()
+        quote=random.choice(lines)
+        return quote
+
+quote=get_quote()
+print(quote)
+# Using SMTP_SSL (port 465) - no need for starttls()
+if day_of_week==1:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="shreyasshende.999@gmail.com",
+            msg=f"Subject:Hello\n\n{quote}."
+        )
+
+
 
