63864bb45859d10175eee1a9991a1262a36267de
shreyass0007 <shreshshende.777@gmail.com>
2025-07-06 12:52:44 +0530

    ading text on cnavas

--- day34/quizzler-app-start/main.py
+++ day34/quizzler-app-start/main.py
@@ -12,7 +12,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quize_ui=QuizeInterface()
+quize_ui=QuizeInterface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
