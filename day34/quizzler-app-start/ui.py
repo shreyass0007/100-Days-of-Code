a8291da4936d4bd20859ea4e73015d3f7860dd81
shreyass0007 <shreshshende.777@gmail.com>
2025-07-06 12:52:44 +0530

    ading text on cnavas

--- day34/quizzler-app-start/ui.py
+++ day34/quizzler-app-start/ui.py
@@ -1,9 +1,11 @@
 from tkinter import*
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizeInterface:
-    def __init__(self):
+    def __init__(self,quiz_brain:QuizBrain):
+        self.quiz=quiz_brain
         self.window=Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20,pady=20,bg=THEME_COLOR)
@@ -15,6 +17,8 @@ class QuizeInterface:
 
         self.canvas=Canvas(width=300,height=250,bg="white")
         self.question_text=self.canvas.create_text(150,125,
+                                                   width=280,
+                                                   
                                                    text="Some Question Text",
                                                    fill=THEME_COLOR,
                                                    font=("Arial",20,"italic")
@@ -28,6 +32,10 @@ class QuizeInterface:
         self.false_button=Button(image=false_image,highlightthickness=0)
         self.false_button.grid(row=2,column=1)
 
-
+        self.get_next_question()
         self.window.mainloop()
         
+    
+    def get_next_question(self):
+        q_text=self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text,text=q_text)
