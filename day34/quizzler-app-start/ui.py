45976aa51732e92bea58046dd883324bf75e0cd8
shreyass0007 <shreshshende.777@gmail.com>
2025-07-06 11:29:40 +0530

    creating tkinter UI

--- day34/quizzler-app-start/ui.py
+++ day34/quizzler-app-start/ui.py
@@ -1,2 +1,13 @@
+from tkinter import*
+
 THEME_COLOR = "#375362"
 
+class QuizeInterface:
+    def __init__(self):
+        self.window=Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20,bg=THEME_COLOR)
+        
+
+        self.window.mainloop()
+        
