01f58b01bcc349a7dce33ecf4f6bc5ace35ddacc
shreyass0007 <shreshshende.777@gmail.com>
2025-07-06 11:09:12 +0530

    update

--- day34/quizzler-app-start/data.py
+++ day34/quizzler-app-start/data.py
@@ -1,8 +1,15 @@
 import requests
-response=requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
+parameters={
+    "amount":10,
+    "type":"boolean",
+
+}
+response=requests.get("https://opentdb.com/api.php?amount=10&type=boolean",params=parameters)
 response.raise_for_status()
 data=response.json()
+
 question_data=data["results"]
+# print(question_data)
 
 
 
