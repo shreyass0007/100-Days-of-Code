8ca4ddc85653f692cbec2aeaa736b90e4a260487
shreyass0007 <shreshshende.777@gmail.com>
2025-07-07 09:59:14 +0530

    update

--- day35/main.py
+++ day35/main.py
@@ -7,7 +7,20 @@ PARAMETER={
     "lat":MY_LAT,
     "lon":MY_LON,
     "appid":API_KEY,
+    "cnt":4,
 }
 response=requests.get(OWM_ENDPOINT,params=PARAMETER)
-print(response.json())
+response.raise_for_status()
+weather_data=response.json()
+#print(weather_data["list"][0]["weather"][0]["id"])
+will_rain=False
+for hour_data in weather_data["list"]:
+    condition_code=hour_data["weather"][0]["id"]
+    if int(condition_code) <700:
+        wil_rain=True
+
+if will_rain:
+    print("Bring an Umbrella")
+else:
+    print("No Rain Today")
 
