ee1fe73276d9383ebe8526690d04d10f09790718
shreyass0007 <shreshshende.777@gmail.com>
2025-07-10 09:52:06 +0530

    scraping Yc live web

--- day45/bs4-start/bs4-start/main.py
+++ day45/bs4-start/bs4-start/main.py
@@ -2,4 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 response=requests.get("https://appbrewery.github.io/news.ycombinator.com/")
 
-print(response.text.format())
+yc_web_page=response.text
+
+soup=BeautifulSoup(yc_web_page,"html.parser")
+print(soup.title)
