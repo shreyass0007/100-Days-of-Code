2d9e85b6b03295c3238d7e57a667d42bf2c360b7
shreyass0007 <shreshshende.777@gmail.com>
2025-07-10 11:15:25 +0530

    web Scriping 1st prj

--- day45/bs4-start/bs4-start/main.py
+++ day45/bs4-start/bs4-start/main.py
@@ -5,11 +5,27 @@ response=requests.get("https://appbrewery.github.io/news.ycombinator.com/")
 yc_web_page=response.text
 
 soup=BeautifulSoup(yc_web_page,"html.parser")
-print(soup.title)
 
-article_tag=soup.find(name="a",class_="storylink")
-article_link=soup.find(name="a")
-article_upvote=soup.find(name="span",class_="score",id="score_40725924")
-print(article_tag.get_text())
-print(article_link.get("href"))
-print(article_upvote.get_text())
+
+articles=soup.find_all(name="a",class_="storylink")
+article_texts=[]
+article_links=[]
+article_upvotes=[]
+for artical_tag in articles:
+    text=artical_tag.getText()
+    article_texts.append(text)
+    link=artical_tag.get("href")
+    article_links.append(link)
+article_upvote=soup.find_all(name="span",class_="score")
+
+print(article_texts)
+print(article_links)
+for score in article_upvote:
+    upvotes=(int(score.getText().split(" ")[0]))
+    article_upvotes.append(upvotes)
+
+max_value=max(article_upvotes)
+max_index=article_upvotes.index(max_value)
+print(article_texts[max_index])
+print(article_links[max_index])
+print(article_upvotes[max_index])
