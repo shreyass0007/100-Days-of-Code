00650d84580742d68fcc88273d7b9c65f48d59a4
shreyass0007 <shreshshende.777@gmail.com>
2025-07-10 13:52:11 +0530

    stock alert prj complete

--- day36/stock-news-normal-start/main.py
+++ day36/stock-news-normal-start/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -6,8 +7,10 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
-STOCK_API_KEY="EVS40MMFA90XC4UG"
-
+STOCK_API_KEY="0UIO16OEBG6SF0VE"
+NEWS_API_KEY="ce1f6558309a4727aaacd186c8638115"
+TWILID_SID=""
+TWILID_AUTH=""
 
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
@@ -27,40 +30,59 @@ data_list=[value for (key,value) in data.items()]
 yesterday_data=data_list[0]
 yesterday_closing_price=yesterday_data["4. close"]
 
-print(yesterday_closing_price)
+# print(yesterday_closing_price)
 #TODO 2. - Get the day before yesterday's closing stock price
 
 day_before_yesterday_data=data_list[1]
 day_before_yesterday_closing_price=day_before_yesterday_data["4. close"]
 
-print(day_before_yesterday_closing_price)
+# print(day_before_yesterday_closing_price)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-difference=abs(float(yesterday_closing_price)-float(day_before_yesterday_closing_price))
-print(difference)
+difference=(float(yesterday_closing_price)-float(day_before_yesterday_closing_price))
+# print(difference)
+up_down=None
+if difference >0:
+    up_down="🔺"
+else:
+    up_down="🔻"
 
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent=(difference / float(yesterday_closing_price))*100
-print(diff_percent)
+diff_percent=round(difference / float(yesterday_closing_price))*100
+# print(diff_percent)
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent>0.60:
-    print("Get News")
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
+if diff_percent>0.6:
+    news_params={
+        "apikey":NEWS_API_KEY,
+        "qInTitle":COMPANY_NAME ,
+    }
+    news_response=requests.get(NEWS_ENDPOINT,params=news_params)
+    articles=news_response.json()["articles"]
+    three_articles=articles[:3]
+    # print(articles)
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+    
+# print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
+formatted_articles= [f"{STOCK_NAME}:{up_down}{diff_percent}%\n\nHeadline:{article['title']}.\n\nBrief:{article['description']}" for article in three_articles]
 #TODO 9. - Send each article as a separate message via Twilio. 
+client=Client(TWILID_SID,TWILID_AUTH)
+for article in formatted_articles:
 
-
+    message=client.messages.create(
+        body=article,
+        from_="",
+        to="",
+    )
 
 #Optional TODO: Format the message like this: 
 """
