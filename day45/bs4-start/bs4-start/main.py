564e5a1ce1992b00f75e60bab9dc8f0bb738843c
shreyass0007 <shreshshende.777@gmail.com>
2025-07-10 10:03:26 +0530

    completing challenges

--- day45/bs4-start/bs4-start/main.py
+++ day45/bs4-start/bs4-start/main.py
@@ -6,3 +6,10 @@ yc_web_page=response.text
 
 soup=BeautifulSoup(yc_web_page,"html.parser")
 print(soup.title)
+
+article_tag=soup.find(name="a",class_="storylink")
+article_link=soup.find(name="a")
+article_upvote=soup.find(name="span",class_="score",id="score_40725924")
+print(article_tag.get_text())
+print(article_link.get("href"))
+print(article_upvote.get_text())
