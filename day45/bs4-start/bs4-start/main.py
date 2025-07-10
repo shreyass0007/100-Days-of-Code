f85c8f1b70bd596b2f074846bf9a859941713851
shreyass0007 <shreshshende.777@gmail.com>
2025-07-10 09:38:02 +0530

    scrapping live website

--- day45/bs4-start/bs4-start/main.py
+++ day45/bs4-start/bs4-start/main.py
@@ -1,24 +1,5 @@
 from bs4 import BeautifulSoup
-import lxml
+import requests
+response=requests.get("https://appbrewery.github.io/news.ycombinator.com/")
 
-
-with open("day45/bs4-start/bs4-start/website.html") as file:
-    contents=file.read()
-
-soup=BeautifulSoup(contents,"html.parser")
-print(soup.title.string)
-# print(soup.prettify())
-all_anchor_tags=soup.find(name="p")
-
-# for tag in all_anchor_tags:
-#     print(tag.get("href"))
-
-
-heading=soup.find(name="h1",id="name")
-print(heading.get_text())
-
-section_heading=soup.find(name="h3",class_="heading")
-print(section_heading.get("class"))
-
-
-print(soup.select(".heading"))
+print(response.text.format())
