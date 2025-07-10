ec96b00141676ea29e8f631e172b3327eebd776e
shreyass0007 <shreshshende.777@gmail.com>
2025-07-10 11:35:57 +0530

    printing movies name form web

--- day45/bs4-start/Starting+Code+-+100+movies+to+watch+start/Starting Code - 100 movies to watch start/main.py
+++ day45/bs4-start/Starting+Code+-+100+movies+to+watch+start/Starting Code - 100 movies to watch start/main.py
@@ -4,5 +4,15 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response=requests.get(URL)
+TOP_MOIVES=response.text
 
+soup=BeautifulSoup(TOP_MOIVES,"html.parser")
+movie_titles=[]
+movie_title=soup.find_all(name="h3",class_="title")
 
+for title in movie_title:
+    name=title.get_text()
+    movie_titles.append(name)
+
+print(movie_titles)
