41571071dbda8a48e744e388ea58bd8a0e4bfb80
shreyass0007 <shreshshende.777@gmail.com>
2025-07-11 11:01:18 +0530

    update

--- day37/main.py
+++ day37/main.py
@@ -4,13 +4,59 @@
 #request.post()
 #request.delete()
 import requests
+from datetime import datetime
+
+USERNAME="shresh"
+TOKEN="abcdefghijklmnop"
+
 PIXELA_END_POINT="https://pixe.la/v1/users"
+GRAPH_ID="graph1"
 user_params={
     "token":"abcdefghijklmnop",
-    "username":"shresh07",
+    "username":"shresh",
     "agreeTermsOfService":"yes",
     "notMinor":"yes"
 
 }
-response=requests.post(url=PIXELA_END_POINT,json=user_params)
+graph_endpoint=f"{PIXELA_END_POINT}/{USERNAME}/graphs"
+graph_config={
+    "id":"graph1",
+    "name":"Reading Graph",
+    "Unit":"pages",
+    "type":"float",
+    "color":"sora"
+
+}
+headers={
+    "X-USER-TOKEN":TOKEN
+
+}
+
+# response=requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+
+PIXEL_CREATION_ENDPOINT=f"{PIXELA_END_POINT}/{USERNAME}/graphs/{GRAPH_ID}"
+today=datetime.now()
+print(today.strftime("%Y%m%d"))
+pixel_data={
+    "date":today.strftime("%Y%m%d"),
+    "quantity":input("How many pages did you read today:"),
+
+
+}
+
+
+response=requests.post(url=PIXEL_CREATION_ENDPOINT,json=pixel_data,headers=headers)
 print(response.text)
+
+UPDATE_END_POINT=f"{PIXELA_END_POINT}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+update_data={
+    "quantity":"1.0"
+}
+
+# response=requests.put(url=UPDATE_END_POINT,json=update_data,headers=headers)
+# print(response.text)
+
+DELETE_END_POINT=f"{PIXELA_END_POINT}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+# response=requests.delete(url=DELETE_END_POINT,headers=headers)
+# print(response.text)
